74b7feb57d2f5d10f8aa5a8c4085159c1757b901
CodingForEntrepreneurs <hello@teamcfe.com>
2023-01-24 11:08:54 -0600

    6 - Django Templates and Home View

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cfehome.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,7 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
 ]
 
