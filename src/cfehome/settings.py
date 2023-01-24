f178e60eaa5943e02231b6b1cc1f662a19352e4b
CodingForEntrepreneurs <hello@teamcfe.com>
2023-01-24 11:01:10 -0600

    5 - Configure Django Static Files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -116,6 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    # "/Users/cfe/Dev/django-tailwindcss/src/static",
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = BASE_DIR.parent / "local-cdn" / "static"
+# STATIC_ROOT = os.path.join(str(BASE_DIR.parent), "local-cdn", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    # do not do this in prod
+    from django.conf.urls.static import static
+    # Try Django
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
