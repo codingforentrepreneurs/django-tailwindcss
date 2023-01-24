92f9a2d8b23e353e683ff13bf379d0af6c33133f
CodingForEntrepreneurs <hello@teamcfe.com>
2023-01-24 12:52:03 -0600

    9 - Colors and Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      white: colors.white,
+      black: colors.black,
+      cfe: "#007cae",
+      cfeBlue: {
+        100: "#007cad"
+      },
+      stone: colors.stone,
+      sky: colors.sky,
+      violet: colors.violet,
+    },
     extend: {},
   },
   plugins: [],
